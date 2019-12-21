5604d43fa5e2d45554e29789e49e0e476d6d0081
dario <dario.x.busco@gmail.com>
2019-12-21 18:50:11 +0100

    slight performance improvement, led blink at each turn

--- shaft_emulator.ino
+++ shaft_emulator.ino
@@ -1,27 +1,45 @@
 #include <TimerOne.h>
 
-const unsigned int TEETH_PRESENT = 11;
+// number of teeth on wheel
+const unsigned int TEETH_PRESENT = 35;
+
+// number of missing teeth on wheel, contiguous
 const unsigned int TEETH_MISSING = 1;
+
+// total number of teeth and gaps
 const unsigned int TEETH_TOTAL = TEETH_PRESENT + TEETH_MISSING;
 
-const unsigned long ACCELERATION_RATE = 1000L * 100L;  // microseconds between each rpm step
-const unsigned int ACCELERATION_STEP = 1;              // rpm increment step
+// angular degrees per tooth position
+const float DEGREES_PER_TOOTH = 360 / (TEETH_TOTAL * 2);
 
-const float RPM_MIN = 15.0;
-const float RPM_MAX = 15.0;
+// microseconds between each rpm step
+const unsigned long ACCELERATION_RATE = 1000L * 50L;
 
-const int OUTPUT_PIN = 13;
+// rpm increment step
+const unsigned int ACCELERATION_STEP = 1;
 
+// rpm min and max
+const float RPM_MIN = 200.0;
+const float RPM_MAX = 500.0;
+
+// used pins
+const int OUTPUT_PIN = 12;
+const int LED_PIN = 13;
+
+// instant rpm
 volatile int rpm = RPM_MIN;
+
+// whether in deceleration phase
 volatile bool falling = false;
 
+// current tooth transitioning
 int currentTooth = -1;
 
 void setup() {
   Serial.begin(9600);
 
   Timer1.initialize(ACCELERATION_RATE); // time in us
-  Timer1.attachInterrupt(changeRPMs);
+  Timer1.attachInterrupt(changeRpm);
 
   pinMode(OUTPUT_PIN, OUTPUT);
 }
@@ -30,6 +48,8 @@ void loop() {
 
   int position = rotateGearForward();
   int transitionTimeMs = computeTransitionTimeMs();
+
+  digitalWrite(LED_PIN, position == 0 ? HIGH : LOW);
   
   if (position < TEETH_PRESENT) {  
     
@@ -63,40 +83,33 @@ int rotateGearForward() {
 }
 
 /**
- * calculates how long a tooth or a gap transit lasts in time at the current rpms
+ * calculates how long a tooth or a gap transit lasts in time at the current rpm
  */
 float computeTransitionTimeMs() {
+
   float rps = rpm / 60.0;
   float revTimeMs = 1000.0 / rps;
   float timeDegMs = revTimeMs / 360.0;
-  float toothDeg = 360 / (TEETH_TOTAL * 2);
-  float transTimeMs = timeDegMs * toothDeg;
-/*
-  Serial.print("rpm: ");
-  Serial.println(rpm);
-  Serial.print("rps: ");
-  Serial.println(rps);
-  Serial.print("revTimeMs: ");
-  Serial.println(revTimeMs);
-  Serial.print("timeDegMs: ");
-  Serial.println(timeDegMs);
-  Serial.print("toothDeg: ");
-  Serial.println(toothDeg);
-  Serial.print("transitionMs: ");
-  Serial.println(transTimeMs);
-*/
+  float transTimeMs = timeDegMs * DEGREES_PER_TOOTH;
   return transTimeMs;
+
 }
 
 /**
- * steps up or down the current rpms
+ * steps up or down the current rpm
  */
-void changeRPMs() {
+void changeRpm() {
+
   if (falling) {
+    
     if (rpm > RPM_MIN) rpm -= ACCELERATION_STEP;
     else falling = false;    
+  
   } else {
+  
     if (rpm < RPM_MAX) rpm += ACCELERATION_STEP;
     else falling = true;
+    
   }
+  
 }
