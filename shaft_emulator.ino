5f7c5eb308709c63015878d1e048606d813b1a2a
dario <dario.x.busco@gmail.com>
2019-12-21 09:32:57 +0100

    fixed missing tooth bug

--- shaft_emulator.ino
+++ shaft_emulator.ino
@@ -1,22 +1,21 @@
 #include <TimerOne.h>
 
-const unsigned int TEETH_PRESENT = 3;
-// todo bug - broken if zero missing teeth
+const unsigned int TEETH_PRESENT = 11;
 const unsigned int TEETH_MISSING = 1;
 const unsigned int TEETH_TOTAL = TEETH_PRESENT + TEETH_MISSING;
 
 const unsigned long ACCELERATION_RATE = 1000L * 100L;  // microseconds between each rpm step
 const unsigned int ACCELERATION_STEP = 1;              // rpm increment step
 
-const float RPM_MIN = 60.0;
-const float RPM_MAX = 60.0;
+const float RPM_MIN = 15.0;
+const float RPM_MAX = 15.0;
 
 const int OUTPUT_PIN = 13;
 
 volatile int rpm = RPM_MIN;
 volatile bool falling = false;
 
-int currentTooth = 1;
+int currentTooth = -1;
 
 void setup() {
   Serial.begin(9600);
@@ -28,30 +27,44 @@ void setup() {
 }
 
 void loop() {
+
+  int position = rotateGearForward();
   int transitionTimeMs = computeTransitionTimeMs();
-  if (currentTooth <= TEETH_PRESENT) {
-    // emulate transit of a tooth:
-    // produce a tooth followed by a gap
+  
+  if (position < TEETH_PRESENT) {  
+    
+    // emulate transit of a present tooth: produce a tooth followed by a gap
+    
     digitalWrite(OUTPUT_PIN, HIGH);
     delay(transitionTimeMs);
     digitalWrite(OUTPUT_PIN, LOW);
     delay(transitionTimeMs);
-    currentTooth++;
-  } else if (currentTooth <= TEETH_TOTAL) {
-    // emulate transit of a missing tooth:
-    // produce twice the gap
+
+  } else {
+    
+    // emulate transit of a missing tooth: produce twice the gap
+    
     digitalWrite(OUTPUT_PIN, LOW);
     delay(transitionTimeMs * 2);
-    if (currentTooth == TEETH_TOTAL) {
-      // last tooth, back to start
-      currentTooth = 1;
-    } else {
-      // intermediate gap tooth, increment
-      currentTooth++;      
-    }
+
+  }
+
+}
+
+/**
+ * moves the gear one tooth forward or cycles back at the end of the turn
+ */
+int rotateGearForward() {
+  if (currentTooth == TEETH_TOTAL-1) {
+    return currentTooth = 0;
+  } else {
+    return ++currentTooth;  
   }
 }
 
+/**
+ * calculates how long a tooth or a gap transit lasts in time at the current rpms
+ */
 float computeTransitionTimeMs() {
   float rps = rpm / 60.0;
   float revTimeMs = 1000.0 / rps;
@@ -75,6 +88,9 @@ float computeTransitionTimeMs() {
   return transTimeMs;
 }
 
+/**
+ * steps up or down the current rpms
+ */
 void changeRPMs() {
   if (falling) {
     if (rpm > RPM_MIN) rpm -= ACCELERATION_STEP;
@@ -83,5 +99,4 @@ void changeRPMs() {
     if (rpm < RPM_MAX) rpm += ACCELERATION_STEP;
     else falling = true;
   }
-//  Serial.println(rpm);
 }
